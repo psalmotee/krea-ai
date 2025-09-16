83f88d7edefc17aeb78f919a45670889658de9fb
psalmotee <samsonmoradeyo@gmail.com>
2025-09-16 10:01:08 +0100

    update layout
    Deleted unused image and icon files, removed obsolete utility script, and updated layout.tsx to reflect these changes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type React from "react";
 import type { Metadata } from "next";
-// import { GeistSans } from "geist/font/sans";
-// import { GeistMono } from "geist/font/mono";
 import { ThemeProvider } from "next-themes";
 import { Suspense } from "react";
 import "./globals.css";
@@ -9,7 +7,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Krea AI",
   description: "Discover and explore the best AI tools",
-  generator: "v0.app",
    icons: {
     icon: "/image/Krea-logo-black.png",
   },
